0733f9ee4e860eae95118cec7e621eeda472f677
yinhaiying <18840842571@163.com>
2019-05-05 16:08:52 +0800

    中间件

--- app.js
+++ app.js
@@ -5,12 +5,30 @@ let app = new Koa();
 let Router = require('koa-router');
 let router = new Router();
 //中间件
+app.use(async (ctx,next) => {
+  ctx.body = '这是一个中间件'
+  console.log(1)
+  await next(); // 在这里执行时出现错误。
+  // 进行错误处理
+  if(ctx.status == 404){
+    ctx.body = '404 page';
+    console.log(2)
+  }
+})
+
+
+
+
 // ctx 替代了express中的res和res
 router.get('/', async (ctx,next) => {
     // res.send('返回数据')
     ctx.body = 'router';// ctx.body返回数据
 })
 
+router.use('/test',async (ctx,next) => {
+    console.log('路由级中间件匹配完成之后继续往下执行');
+    await next();
+})
 router.get('/test', async (ctx,next) => {
     ctx.body = '这是一个测试';
 })
